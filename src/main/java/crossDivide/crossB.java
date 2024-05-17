8f678056dc1c7e08150e90d839702da2cab72485
Laurent Le Bourhis <laurentl@synopsys.com>
2024-05-17 12:46:39 +0000

    Add new file

---
+++ src/main/java/crossDivide/crossB.java
@@ -0,0 +1,12 @@
+package crossDivide;
+
+public class crossB {
+  public int returnZeroSometimes(int a)
+  {
+        if (a!=0) {
+           //Do something
+
+        }
+        return 0;
+  }  
+}
