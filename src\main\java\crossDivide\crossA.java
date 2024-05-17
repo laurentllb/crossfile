10e17fd203c226a40e989dda88decdf5bab634eb
Laurent Le Bourhis <laurentl@synopsys.com>
2024-05-17 12:44:45 +0000

    Add new file

---
+++ src\main\java\crossDivide\crossA.java
@@ -0,0 +1,10 @@
+package crossDivide;
+public class crossA {
+  public static void main(String[] args) {
+	int a = 15;
+    crossB cross = new crossB();
+	int result = 25 / cross.returnZeroSometimes(a);
+  }
+}
+
+
